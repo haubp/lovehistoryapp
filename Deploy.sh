ad070672f4357c2f69edb5d946459361772ed658
haubp <buiphonghau2018@gmail.com>
2024-02-18 23:32:05 +0700

    deploy

--- Deploy.sh
+++ Deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# cd to Web App directory
 cd LoveHistoryWebApp
 
 # Send request to download LoveInfo.json
@@ -11,7 +12,13 @@ dotnet build --configuration Release
 # Publish project in release mode
 dotnet publish --configuration Release
 
+# cd to publish directory
 cd bin/Release/net7.0/publish
 
 # Execute deploy to Azure command
 az webapp up --name test-dotnet-my-webapp --resource-group test-rg --runtime "DOTNET|8"
+
+# Commit and push changes to GitHub after deploy
+git add -A
+git commit -m "deploy"
+git push
