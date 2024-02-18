17c36862e495a2cf806277f34c121cf217e625e1
haubp <buiphonghau2018@gmail.com>
2024-02-18 23:28:29 +0700

    update deploy script to deploy to azure

--- Deploy.sh
+++ Deploy.sh
@@ -11,5 +11,7 @@ dotnet build --configuration Release
 # Publish project in release mode
 dotnet publish --configuration Release
 
+cd bin/Release/net7.0/publish
+
 # Execute deploy to Azure command
-az webapp up --sku F1 --name test-dotnet-my-webapp --resource-group test-rg
+az webapp up --name test-dotnet-my-webapp --resource-group test-rg --runtime "DOTNET|8"
